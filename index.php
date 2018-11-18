fba3cc6fa965f593387d8ef47125b2750a5db922
abdullahnaem <naemislam54321@gmail.com>
2018-11-18 22:37:40 +0600

    create layout on index.html file

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include_once 'header.php'; ?>	
+
+  <div class="panel panel-defoult container ">
+    <div class="panel-heading">
+      <h2>User List <span class="float-right"><strong>Welcome! Naem</strong></span></h2>
+    </div>
+
+    <div class="panel-body">
+      <table class="table table-striped">
+        <thead>
+          <th width="20%">Serial</th>
+          <th width="20%">Name</th>
+          <th width="20%">UserName</th>
+          <th width="20%">Email Address</th>
+          <th width="20%">Action</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>Naem Islam</td>
+            <td>Naem</td>
+            <td>sdfjds@gmail.com</td>
+            <td><a href="#" class="btn btn-primary">View</a></td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>Bulbul Islam</td>
+            <td>Bablu</td>
+            <td>asdfdsf@gmail.com</td>
+            <td><a href="#" class="btn btn-primary">View</a></td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>Bulbul Ahmed</td>
+            <td>Bulbul</td>
+            <td>eeeddf@gmail.com</td>
+            <td><a href="#" class="btn btn-primary">View</a></td>
+          </tr>
+          <tr>
+            <td>04</td>
+            <td>Shohel Ahmed</td>
+            <td>Shohel</td>
+            <td>shohel@gmail.com</td>
+            <td><a href="#" class="btn btn-primary">View</a></td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+	include_once 'footer.php';
+ ?>
